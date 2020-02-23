a2a38ac03b1f2d9b247e37ce667cdf7a5c66997e
Maya <mmk58023@creighton.edu>
2020-02-23 14:56:09 -0600

    new
    new

---
+++ forms/calcAB/calcAB.js
@@ -0,0 +1,20 @@
+/*
+
+let nums = [123, 433, 54326, 2234, 1919, 1123, 1928, 3374, 23, 190, 33874]
+function ProductCalc(n1,n2){
+  return n1*n2
+}
+
+
+let stopPoint = nums.length
+console.log(stoppingPoint)
+
+let products = []
+let j = 0 
+for (i = 0; i <= stoppingPoint; i+=2) {
+  products[j] = ProductCalc(nums[i],nums[i+1])
+  j = j + 1 
+}
+console.log(products)
+
+*/
